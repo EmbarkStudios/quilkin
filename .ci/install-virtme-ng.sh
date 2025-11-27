c163942d8f23c77ffd4d4d945d5068b8669b2115
Jake Shadle <jake.shadle@embark-studios.com>
2025-11-27 12:41:49 +0100

    Ugh

--- .ci/install-virtme-ng.sh
+++ .ci/install-virtme-ng.sh
@@ -10,5 +10,5 @@ sudo apt-get update && sudo apt-get install python3-pip qemu-system-x86
 echo "::notice file=$source,line=$LINENO::Installing virtme-ng $virtme_version"
 
 git clone https://github.com/arighi/virtme-ng
-(cd virtme-ng && BUILD_VIRTME_NG_INIT=1 pip3 install)
+(cd virtme-ng && BUILD_VIRTME_NG_INIT=1 pip3 install .)
 
