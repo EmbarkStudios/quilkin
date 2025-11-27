e6b754e0e2b533e3854755ede1d36a61cdfef401
Jake Shadle <jake.shadle@embark-studios.com>
2025-11-27 12:52:19 +0100

    Oops

--- .ci/install-virtme-ng.sh
+++ .ci/install-virtme-ng.sh
@@ -12,7 +12,7 @@ echo "::notice file=$source,line=$LINENO::Installing virtme-ng $virtme_version"
 # We need to install virtme-ng from source because the version in ubuntu-24.04
 # is, naturally, super out of date. And we need to clone it outside the source
 # directory, otherwise the quilkin workspace screws it up
-pushd $TEMP
+pushd $RUNNER_TEMP
 git clone https://github.com/arighi/virtme-ng
 (cd virtme-ng && BUILD_VIRTME_NG_INIT=1 pip3 install .)
 popd
