cdc7e54949e8bb551ef5e9cb263858949f5e1e32
Jake Shadle <jake.shadle@embark-studios.com>
2025-11-27 12:36:10 +0100

    Use virtme-ng to test on multiple kernels

---
+++ .ci/install-virtme-ng.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+source="${BASH_SOURCE[0]}"
+virtme_version="v3.18"
+
+echo "::notice file=$source,line=$LINENO::Installing dependencies"
+sudo apt-get update && sudo apt-get install python3-pip qemu-system-x86
+
+echo "::notice file=$source,line=$LINENO::Installing virtme-ng $virtme_version"
+
+git clone https://github.com/arighi/virtme-ng
+(cd virtme-ng && BUILD_VIRTME_NG_INIT=1 pip3 install)
+
