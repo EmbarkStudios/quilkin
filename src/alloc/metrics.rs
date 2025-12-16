4bdee91ec286e09af8c0fbbff27e240544321f5f
Gustaf Lindstedt <gustaflindstedt@protonmail.com>
2025-12-16 11:08:44 +0100

    Export metrics for jemalloc statistics

--- src/alloc/metrics.rs
+++ src/alloc/metrics.rs
@@ -16,7 +16,183 @@
 
 cfg_if::cfg_if! {
     if #[cfg(feature = "jemalloc")] {
-        pub fn spawn_heap_stats_updates(_period: std::time::Duration, _srx: crate::signal::ShutdownRx) {}
+        use prometheus::core::Collector;
+        use tikv_jemalloc_ctl::stats;
+
+        struct JemallocCollector {
+            epoch: tikv_jemalloc_ctl::epoch_mib,
+            active_desc: prometheus::core::Desc,
+            active: tikv_jemalloc_ctl::stats::active_mib,
+            allocated_desc: prometheus::core::Desc,
+            allocated: tikv_jemalloc_ctl::stats::allocated_mib,
+            mapped_desc: prometheus::core::Desc,
+            mapped: tikv_jemalloc_ctl::stats::mapped_mib,
+            metadata_desc: prometheus::core::Desc,
+            metadata: tikv_jemalloc_ctl::stats::metadata_mib,
+            resident_desc: prometheus::core::Desc,
+            resident: tikv_jemalloc_ctl::stats::resident_mib,
+            retained_desc: prometheus::core::Desc,
+            retained: tikv_jemalloc_ctl::stats::retained_mib,
+        }
+
+        impl JemallocCollector {
+            fn new() -> crate::Result<Self> {
+                Ok(Self {
+                    epoch: tikv_jemalloc_ctl::epoch::mib()?,
+                    active_desc: prometheus::core::Desc::new(
+                        "jemalloc_stats_active_bytes".to_string(),
+                        "Total number of bytes in active pages allocated by the application.".to_string(),
+                        Vec::new(),
+                        std::collections::HashMap::new(),
+                    )?,
+                    active: stats::active::mib()?,
+                    allocated_desc: prometheus::core::Desc::new(
+                        "jemalloc_stats_allocated_bytes".to_string(),
+                        "Total number of bytes allocated by the application.".to_string(),
+                        Vec::new(),
+                        std::collections::HashMap::new(),
+                    )?,
+                    allocated: stats::allocated::mib()?,
+                    mapped_desc: prometheus::core::Desc::new(
+                        "jemalloc_stats_mapped_bytes".to_string(),
+                        "Total number of bytes in active extents mapped by the allocator.".to_string(),
+                        Vec::new(),
+                        std::collections::HashMap::new(),
+                    )?,
+                    mapped: stats::mapped::mib()?,
+                    metadata_desc: prometheus::core::Desc::new(
+                        "jemalloc_stats_metadata_bytes".to_string(),
+                        "Total number of bytes dedicated to jemalloc metadata.".to_string(),
+                        Vec::new(),
+                        std::collections::HashMap::new(),
+                    )?,
+                    metadata: stats::metadata::mib()?,
+                    resident_desc: prometheus::core::Desc::new(
+                        "jemalloc_stats_resident_bytes".to_string(),
+                        "Total number of bytes in physically resident data pages mapped by the allocator.".to_string(),
+                        Vec::new(),
+                        std::collections::HashMap::new(),
+                    )?,
+                    resident: stats::resident::mib()?,
+                    retained_desc: prometheus::core::Desc::new(
+                        "jemalloc_stats_retained_bytes".to_string(),
+                        "Total number of bytes in virtual memory mappings that were retained rather than being returned to the operating system via e.g. munmap(2).".to_string(),
+                        Vec::new(),
+                        std::collections::HashMap::new(),
+                    )?,
+                    retained: stats::retained::mib()?,
+                })
+            }
+        }
+
+        fn bytes_gauge_into_metric_family(
+            bytes: usize,
+            desc: &prometheus::core::Desc,
+        ) -> prometheus::proto::MetricFamily {
+            let mut gauge = prometheus::proto::Gauge::default();
+            gauge.set_value(bytes as f64);
+            let mut mf = prometheus::proto::MetricFamily::default();
+            mf.set_name(desc.fq_name.clone());
+            mf.set_help(desc.help.clone());
+            mf.set_field_type(prometheus::proto::MetricType::GAUGE);
+            mf.set_metric(vec![prometheus::proto::Metric::from_gauge(gauge)]);
+            mf
+        }
+
+        impl Collector for JemallocCollector {
+            fn desc(&self) -> Vec<&prometheus::core::Desc> {
+                vec![
+                    &self.active_desc,
+                    &self.allocated_desc,
+                    &self.mapped_desc,
+                    &self.metadata_desc,
+                    &self.resident_desc,
+                    &self.retained_desc,
+                ]
+            }
+
+            fn collect(&self) -> Vec<prometheus::proto::MetricFamily> {
+                // many statistics are cached and only updated when the epoch is advanced.
+                if let Err(error) = self.epoch.advance() {
+                    tracing::warn!(?error, "failed to advance epoch")
+                }
+
+                let mut results = Vec::with_capacity(6);
+
+                if let Ok(active) = self
+                    .active
+                    .read()
+                    .inspect_err(|error| tracing::warn!(?error, "failed to collect jemalloc metric"))
+                {
+                    results.push(bytes_gauge_into_metric_family(active, &self.active_desc));
+                }
+
+                if let Ok(allocated) = self
+                    .allocated
+                    .read()
+                    .inspect_err(|error| tracing::warn!(?error, "failed to collect jemalloc metric"))
+                {
+                    results.push(bytes_gauge_into_metric_family(
+                        allocated,
+                        &self.allocated_desc,
+                    ));
+                }
+
+                if let Ok(mapped) = self
+                    .mapped
+                    .read()
+                    .inspect_err(|error| tracing::warn!(?error, "failed to collect jemalloc metric"))
+                {
+                    results.push(bytes_gauge_into_metric_family(mapped, &self.mapped_desc));
+                }
+
+                if let Ok(metadata) = self
+                    .metadata
+                    .read()
+                    .inspect_err(|error| tracing::warn!(?error, "failed to collect jemalloc metric"))
+                {
+                    results.push(bytes_gauge_into_metric_family(
+                        metadata,
+                        &self.metadata_desc,
+                    ));
+                }
+
+                if let Ok(resident) = self
+                    .resident
+                    .read()
+                    .inspect_err(|error| tracing::warn!(?error, "failed to collect jemalloc metric"))
+                {
+                    results.push(bytes_gauge_into_metric_family(
+                        resident,
+                        &self.resident_desc,
+                    ));
+                }
+
+                if let Ok(retained) = self
+                    .retained
+                    .read()
+                    .inspect_err(|error| tracing::warn!(?error, "failed to collect jemalloc metric"))
+                {
+                    results.push(bytes_gauge_into_metric_family(
+                        retained,
+                        &self.retained_desc,
+                    ));
+                }
+
+                results
+            }
+        }
+
+        pub fn spawn_heap_stats_updates(_period: std::time::Duration, _srx: crate::signal::ShutdownRx) {
+            match JemallocCollector::new() {
+                Ok(collector) => {
+                    if let Err(error) = crate::metrics::registry().register(Box::new(collector)) {
+                        tracing::error!(?error, "failed to register JemallocCollector");
+                    }
+                },
+                Err(error) => tracing::error!(?error, "failed to create JemallocCollector"),
+            }
+        }
     } else if #[cfg(feature = "heap-stats")] {
         /// Spawns a task to periodically update the heap stat metrics from our tracking allocator
         pub fn spawn_heap_stats_updates(period: std::time::Duration, mut srx: crate::signal::ShutdownRx) {
