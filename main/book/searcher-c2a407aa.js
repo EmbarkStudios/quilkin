a3a8d6d1338c4c174c9bd5d20be5d8f92e6b76cd
XAMPPRocky <4464295+XAMPPRocky@users.noreply.github.com>
2026-01-27 12:22:20 +0000

    Deploying to gh-pages from @ EmbarkStudios/quilkin@f04b245d7e0320d50f152ad32521e847cacbbd9e 🚀

--- main/book/searcher-c2a407aa.js
+++ main/book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-b87a947a.js',
+                path_to_root + 'searchindex-7cb82db8.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
